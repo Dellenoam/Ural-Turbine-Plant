a682002afe08170a43986c7148852c5b61b41387
Dmitry Spiridonov <dellenoam@Dellenoam-MacBook.local>
2023-07-30 00:21:29 +0500

    Removed unnecessary lines of code

--- access_office/tasks.py
+++ access_office/tasks.py
@@ -6,15 +6,13 @@ from django.db.models import Q
 
 @app.task
 def daily_check():
-    current_date = datetime.now().date()
-
     documents_to_archive = Document.objects.filter(
-        Q(date_end__lt=datetime.now().date()), ~Q(status='A') | ~Q(status='N')
+        Q(date_end__lt=datetime.now().date()), ~Q(status__in=['A', 'N'])
     )
+
     for document_to_archive in documents_to_archive:
         if document_to_archive.comment_for_security == '':
             document_to_archive.status = 'N'
-            document_to_archive.save()
         else:
             document_to_archive.status = 'A'
-            document_to_archive.save()
+        document_to_archive.save()
